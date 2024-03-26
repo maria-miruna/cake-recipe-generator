a057c2585344edc10537d84e561c1f1c4c12c75c
Botiş Maria <126352756+maria-miruna@users.noreply.github.com>
2024-03-26 15:35:27 +0200

    added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Apple pie recipe",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
+
+let recipeFormElement = document.querySelector("#cake-recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
